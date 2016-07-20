074abc6dbc27fcba6f93600349df6b8f6e6d7fbf
Monir Hossain <mknishad1@ymail.com>
2016-07-21 02:30:45 +0600

    add author list activity

---
+++ app/src/main/java/com/elitedevelopers/ilibrary/AuthorListActivity.java
@@ -0,0 +1,13 @@
+package com.elitedevelopers.ilibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_author_list);
+    }
+}
