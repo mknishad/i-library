13c7b54f080eaadc829bc2b8c910c6ed982a10cc
Monir Hossain <mknishad1@ymail.com>
2016-07-21 15:29:02 +0600

    add functionality for home activity buttons

--- app/src/main/java/com/elitedevelopers/ilibrary/activities/HomeActivity.java
+++ app/src/main/java/com/elitedevelopers/ilibrary/activities/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.elitedevelopers.ilibrary.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.elitedevelopers.ilibrary.R;
 
@@ -11,5 +13,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void gotoAddBookActivity(View view) {
+        Intent addBookIntent = new Intent(this, AddBookActivity.class);
+        startActivity(addBookIntent);
+    }
+
+    public void gotoAuthorListActivity(View view) {
+        Intent authorIntent = new Intent(this, AuthorListActivity.class);
+        startActivity(authorIntent);
+    }
+
+    public void gotoCategoryListActivity(View view) {
+        Intent categoryIntent = new Intent(this, CategoryListActivity.class);
+        startActivity(categoryIntent);
     }
 }
