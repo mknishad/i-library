a5d8dc92936e21a2841547e72ceeea1877c2efdc
ASIF-006 <Professional100001@yahoo.com>
2016-07-23 12:43:19 +0600

    add animation to splash screen

--- app/src/main/java/com/elitedevelopers/ilibrary/activities/SplashScreen.java
+++ app/src/main/java/com/elitedevelopers/ilibrary/activities/SplashScreen.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.elitedevelopers.ilibrary.R;
 
@@ -12,7 +15,7 @@ import com.elitedevelopers.ilibrary.R;
  */
 public class SplashScreen extends Activity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 2000; // set the delay for splash screen
+    private final int SPLASH_DISPLAY_LENGTH = 10000; // set the delay for splash screen
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -30,6 +33,13 @@ public class SplashScreen extends Activity {
                 SplashScreen.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.scale);
+        a.reset();
+        TextView tv = (TextView) findViewById(R.id.tvAppName);
+        tv.clearAnimation();
+        tv.startAnimation(a);
+
     }
 
 }
