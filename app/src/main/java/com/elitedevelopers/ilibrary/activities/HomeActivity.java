08cb1a38869fa3ac2c76815f648caa5487d96832
Monir Hossain <mknishad1@ymail.com>
2016-07-24 19:37:22 +0600

    add back button action for home activity

--- app/src/main/java/com/elitedevelopers/ilibrary/activities/HomeActivity.java
+++ app/src/main/java/com/elitedevelopers/ilibrary/activities/HomeActivity.java
@@ -2,12 +2,16 @@ package com.elitedevelopers.ilibrary.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.elitedevelopers.ilibrary.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private boolean exit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +32,25 @@ public class HomeActivity extends AppCompatActivity {
     public void gotoCategoryListActivity(View view) {
         Intent categoryIntent = new Intent(this, CategoryListActivity.class);
         startActivity(categoryIntent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+        }
     }
 }
