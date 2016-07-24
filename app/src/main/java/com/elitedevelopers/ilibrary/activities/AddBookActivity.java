a9b0c184c3027832e7331fa623c235fa1d6552ac
Rubuya Tabassum <rubuyatabassum09@gmail.com>
2016-07-24 01:09:43 -0700

    add menu

--- app/src/main/java/com/elitedevelopers/ilibrary/activities/AddBookActivity.java
+++ app/src/main/java/com/elitedevelopers/ilibrary/activities/AddBookActivity.java
@@ -2,6 +2,8 @@ package com.elitedevelopers.ilibrary.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -59,5 +61,6 @@ public class AddBookActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Enter information properly", Toast.LENGTH_SHORT).show();
         }
+
     }
 }
--- app/src/main/java/com/elitedevelopers/ilibrary/activities/HomeActivity.java
+++ app/src/main/java/com/elitedevelopers/ilibrary/activities/HomeActivity.java
@@ -3,6 +3,8 @@ package com.elitedevelopers.ilibrary.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.elitedevelopers.ilibrary.R;
@@ -28,5 +30,12 @@ public class HomeActivity extends AppCompatActivity {
     public void gotoCategoryListActivity(View view) {
         Intent categoryIntent = new Intent(this, CategoryListActivity.class);
         startActivity(categoryIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 }
